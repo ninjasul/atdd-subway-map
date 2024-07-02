cff7b5681db3a34532cf021fcdb64ddf22306af3
m5s3 <125719290+m5s3@users.noreply.github.com>
2024-07-02 22:59:26 +0900

    🚀 1단계 - 지하철역 인수 테스트 작성 (#1155)
    * test(RestAssuredTest): 구글 페이지 접근 테스트 작성
    
    * test(StationAcceptance): 모든 지하철의 목록을 조회 하는 테스트 작성
    
    * test(StationAcceptance): 지하철역을 삭제하는 테스트 작성
    
    * fix(StationAcceptanceTest): 인자에 들어가는 값 수정

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -55,13 +55,75 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @DisplayName("모든 지하철역 목록을 조회한다.")
+    @Test
+    void readStations() {
+        // Given
+        createStations();
+        int expectedCountOfStation = 2;
+
+        // When
+        List<String> stationsNames = RestAssured.given().log().all()
+                .when().get("/stations")
+                .then().log().all()
+                .extract().jsonPath().getList("name", String.class);
+
+        // Then
+        assertThat(stationsNames).contains("숙대입구", "서울역");
+        assertThat(stationsNames.size()).isEqualTo(expectedCountOfStation);
+    }
 
     /**
      * Given 지하철역을 생성하고
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("지하철역을 삭제한다")
+    @Test
+    void deleteStation() {
+        // Given
+        List<ExtractableResponse<Response>> stations = createStations();
+        ExtractableResponse<Response> responseExtractableResponse = stations.get(0);
+        Long stationId = responseExtractableResponse.jsonPath().getObject("id", Long.class);
+        String stationName = responseExtractableResponse.jsonPath().getObject("name", String.class);
+        int expectedCountOfStation = 1;
 
+        // When
+        RestAssured.given().log().all()
+                .when()
+                .delete("/stations/" + stationId)
+                .then().log().all();
+
+        // Then
+        List<String> stationsNames = RestAssured.given().log().all()
+                .when().get("/stations")
+                .then().log().all()
+                .extract().jsonPath().getList("name", String.class);
+        assertThat(stationsNames).doesNotContain(stationName);
+        assertThat(stationsNames.size()).isEqualTo(expectedCountOfStation);
+    }
+
+    private List<ExtractableResponse<Response>> createStations() {
+        Map<String, String> stationParamsOfSookmyungEntranceStation = createStationParams("숙대입구");
+        Map<String, String> stationParamsOfSeoulStation = createStationParams("서울역");
+        ExtractableResponse<Response> responseOfSookmyungEntraceStation = requestCreateStation(
+                stationParamsOfSookmyungEntranceStation);
+        ExtractableResponse<Response> responseOfSeoulStation = requestCreateStation(stationParamsOfSeoulStation);
+        return List.of(responseOfSookmyungEntraceStation, responseOfSeoulStation);
+    }
+
+    private Map<String, String> createStationParams(String name) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+        return params;
+    }
+
+    private ExtractableResponse<Response> requestCreateStation(Map<String, String> params) {
+        return RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all()
+                .extract();
+    }
 }
