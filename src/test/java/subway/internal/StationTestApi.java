1a9af4f9956dd3c73340dafce8f7426a4b570185
Kyungseok Lee <leeseok8347@gmail.com>
2024-07-03 12:46:46 +0900

    Step1 - 지하철역 인수 테스트 작성 (#1169)
    * Add parameterizedTest when test createStation
    
    * Add showStations acceptance test
    
    * Extract createStation api
    
    * Extract showStations api
    
    * Add deleteStation acceptance test
    
    * Extract deleteStation api
    
    * Add DatabaseCleaner
    
    * Add parameterized test with deleteStation & showStations

---
+++ src/test/java/subway/internal/StationTestApi.java
@@ -0,0 +1,42 @@
+package subway.internal;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StationTestApi {
+    public static ExtractableResponse<Response> createStation(String name) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+
+        return RestAssured
+                .given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> showStations() {
+        return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> deleteStation(Long id) {
+        return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().delete("/stations/" + id)
+                .then().log().all()
+                .extract();
+    }
+}
